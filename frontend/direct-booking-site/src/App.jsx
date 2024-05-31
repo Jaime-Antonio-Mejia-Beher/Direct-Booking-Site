e90efde03c7435748b1d0da115e2ebb20f5627cb
Jaime Mejia-Beher <jmejiabeher@gmail.com>
2024-05-31 00:23:00 -0600

    updated

--- frontend/direct-booking-site/src/App.jsx
+++ frontend/direct-booking-site/src/App.jsx
@@ -1,15 +1,18 @@
-import './App.css'
-import SignInSide from './components/SignIn.jsx'
-import SignUp from './components/SignUp.jsx'
+import * as React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 function App() {
-
   return (
-    <>
-      <SignInSide />
-      <SignUp />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<SignIn />} /> {/* Default route */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
